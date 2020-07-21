c4014e548145ceba9414a7d8b037a09de9d758eb
NeshGogo <rafaelaguero10@hotmail.com>
2020-07-21 16:12:27 -0400

    Estructuras de contron en Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'store';
+  count = 1;
+  names = ['Rafael', 'Maria', 'Elorent', 'Carlos'];
+
+  addItem(): void{
+    this.names.push(`item ${this.count}`);
+    this.count++;
+  }
+
+  deleteItem(index: number): void{
+    this.names.splice(index, 1);
+  }
 }
