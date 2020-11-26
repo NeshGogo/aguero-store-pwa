43db08a7c518d2a33ac3864ea1c3c4d5c693d68f
Rafael Aguero Báez <raguero@SEIP.GOB.LOCAL>
2020-11-26 14:13:58 -0400

    Enviando actualizaciones a nuestra app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component} from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
   template: '<router-outlet></router-outlet>', // Aqui en vez de llamar un achivo le pasamos el html directamente porque es muy corto.
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Store';
+  constructor(
+    private swUpdate: SwUpdate
+  ){}
+  
+  ngOnInit(){
+    this.updatePWA();
+  }
+
+  updatePWA(): void {
+    this.swUpdate.available
+    .subscribe( value => {
+      console.log('update:', value);
+      window.location.reload();
+    })
+  }
 }
